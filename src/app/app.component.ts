fe3533a5bbb394f043f5b569d319da10d4c77589
Olha Wysocky and Yulia Shidlovskaya <olha@>
2019-04-02 10:59:14 -0700

    add even listners for editing the to-do list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,27 @@ import { Task } from './models/task.models';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  tasks: Task[] =[new Task("Wash car", "Medium"), new Task("Cook dinner", "Low"), new Task("Watch movie", "High")];
+  userName: string = "User";
+  tasks: Task[] =[new Task("Wash car", 1), new Task("Cook dinner", 3), new Task("Watch movie", 2)];
+  selectedTask: Task = null;
 
+  editTask(clickedTask){
+    this.selectedTask =  clickedTask;
+    console.log(this.tasks);
+  }
+
+  finishedEditing(){
+    this.selectedTask = null;
+  }
+
+  priorityColor(currentTask){
+    if(currentTask.priority == 3){
+      return "bg-danger";
+    } else if(currentTask.priority == 2){
+      return "bg-warning";
+    }else{
+      return "bg-info";
+    }
+  }
 
 }
--- src/app/models/task.models.ts
+++ src/app/models/task.models.ts
@@ -1,5 +1,5 @@
 export class Task{
-  done: boolean = false;
+  public done: boolean = false;
 
-  constructor(public description: string, public priority: string){}
+  constructor(public description: string, public priority: number){}
 }
